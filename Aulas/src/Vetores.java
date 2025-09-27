d2443a548e6738c2e32663f47f0f15aee2269c6f
Ana Amaral <anacarolinesousadoamaral2020@gmail.com>
2025-09-27 19:16:48 -0300

    Vetores em Java

--- Aulas/src/Vetores.java
+++ Aulas/src/Vetores.java
@@ -1,2 +1,17 @@
 public class Vetores {
+    public static void main(String[] args) {
+        int[] numeros = new int[3];
+
+        numeros[0] = 10;
+        numeros[1] = 20;
+        numeros[2] = 30;
+
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = i + 1;
+        }
+
+        for(int num : numeros){
+            System.out.println(num);
+        }
+    }
 }
