aed6bbe02a34a5286e804a9b8adf22deca959332
Ana Amaral <anacarolinesousadoamaral2020@gmail.com>
2026-01-10 07:13:48 -0300

    Update UsuarioRestController.java

--- ModuloII/spring-api-rest/src/main/java/tech/ada/java/springapirest/api/UsuarioRestController.java
+++ ModuloII/spring-api-rest/src/main/java/tech/ada/java/springapirest/api/UsuarioRestController.java
@@ -1,18 +1,47 @@
 package tech.ada.java.springapirest.api;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioRestController {
 
+    private final List<Usuario> usuarioList = new ArrayList<>();
+
     @GetMapping("/dummy")
     public Usuario dummyUsuario(){
         return new Usuario(UUID.randomUUID(), "Ana", "ana.caroline@gmail.com", LocalDate.now());
     }
+
+    //CRUD - Create, Read, Update, Delete
+    //HTTP - POST, GET, PUT, PATCH, DELETE
+
+
+
+    @GetMapping
+    public List<Usuario> listarTodos(){
+        return this.usuarioList;
+    }
+
+    @GetMapping("/{uuid}")
+    public Usuario buscarPorUuid(@PathVariable UUID uuid){
+        return usuarioList.stream()
+                .filter(usuario -> usuario.getUuid().equals(uuid))
+                .findFirst()
+                .orElseThrow();
+    }
+
+    public Usuario
+
+    @PostMapping("/create-dummy")
+    public Usuario createDummy(){
+        Usuario dummy = new Usuario(UUID.randomUUID(), "Dummy", "dummy@example.com", LocalDate.now());
+        return this.criarUsuario(dummy);
+    }
+
 }
